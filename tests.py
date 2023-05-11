13a5c436afc9608969a7cfc8b4c42c6db1fa9a5b
redfrobro@yahoo.com <Zeppelin1>
2023-05-11 12:20:17 -0500

    adding tests

--- tests.py
+++ tests.py
@@ -6,9 +6,11 @@ import agent
 
 def agent_test():
     """test the agent class"""
-    test_agent = agent.Agent()
+    test_agent = agent.Agent('tester_mctest', 'COSMIC')
     assert test_agent.credits == 0
     assert test_agent.name == "tester_mctest"
+    failed_agent = agent.Agent()
+    assert failed_agent is None
 
 
 
